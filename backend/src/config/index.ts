cb796f420fd8f2502dafde066a5edc8b51fcd47c
MStrong <59189994+mstrong-tech@users.noreply.github.com>
2022-07-02 12:27:04 -0400

    Add .env.template

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -6,8 +6,6 @@ export const config = {
   rpcUrl:
     process.env.RPC_URL ||
     'https://mainnet.infura.io/v3/30bba45435ad4225a9c4db8fbe324e8b',
-  // signer: process.env.SIGNER,
-  // privateKey: process.env.PRIVATE_KEY,
   swapContract:
     process.env.SWAP_CONTRACT || '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D',
   syncDelay: parseInt(process.env.SYNC_DELAY || '20000', 10),
